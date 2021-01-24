f9befab8b7a2ceda833f4bd8da9aa502db7432aa
Hayali <salimovhayali@gmail.com>
2021-01-24 17:04:42 +0300

    Add project files.

---
+++ faction_capital_change/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace faction_capital_change {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
